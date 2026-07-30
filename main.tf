9743cda17cb44e1f8bcae1e92cc5c50fd533d8a8
aryaandevops <aryaandhole@gmail.com>
2026-07-30 17:42:26 +0530

    Refactor AWS instance configuration in main.tf
    Removed existing AWS provider configuration and variables. Added a new AWS instance resource with a specific AMI and instance type.

--- main.tf
+++ main.tf
@@ -1,63 +1,12 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.region
+region = "us-east-1"
 }
 
-variable "region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "instance_count" {
-  description = "Number of EC2 instances to create"
-  type        = number
-  default     = 2
-}
-
-# Find the latest Amazon Linux 2 AMI in the target region
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "web" {
-  count         = var.instance_count
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "terraform-web-${count.index}"
-  }
-}
-
-output "instance_ids" {
-  description = "IDs of the created EC2 instances"
-  value       = aws_instance.web[*].id
-}
-
-output "public_ips" {
-  description = "Public IPs of the created EC2 instances (if assigned)"
-  value       = aws_instance.web[*].public_ip
+resource "aws_instance" "one" {
+    count = 2
+ami = "ami-02b64aa047cb5edf5"
+instance_type = "t3.micro"
+tags = {
+ Name = "aryan-${count.index+1}"
+ }
 }
