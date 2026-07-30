07750274c59973dadf4cc71305665adb0b9fad42
aryaandevops <aryaandhole@gmail.com>
2026-07-30 17:38:41 +0530

    Replace null_resource with AWS EC2 instances (count = 2) and provider configuration

--- main.tf
+++ main.tf
@@ -1,13 +1,63 @@
-// Created a single Terraform resource defined with count = 2 using null_resource
-resource "null_resource" "example" {
-  count = 2
+terraform {
+  required_version = ">= 1.0.0"
 
-  triggers = {
-    instance = "example-${count.index}"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
 
-output "null_resource_ids" {
-  description = "IDs of the created null resources (count = 2)"
-  value       = [for r in null_resource.example : r.id]
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 2
+}
+
+# Find the latest Amazon Linux 2 AMI in the target region
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web" {
+  count         = var.instance_count
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "terraform-web-${count.index}"
+  }
+}
+
+output "instance_ids" {
+  description = "IDs of the created EC2 instances"
+  value       = aws_instance.web[*].id
+}
+
+output "public_ips" {
+  description = "Public IPs of the created EC2 instances (if assigned)"
+  value       = aws_instance.web[*].public_ip
 }
