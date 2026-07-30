42f9b7054bf96a43d2071a0fc9f35a98191536a1
aryaandevops <aryaandhole@gmail.com>
2026-07-30 17:31:38 +0530

    Add null_resource with count = 2 and output

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+// Created a single Terraform resource defined with count = 2 using null_resource
+resource "null_resource" "example" {
+  count = 2
 
+  triggers = {
+    instance = "example-${count.index}"
+  }
+}
+
+output "null_resource_ids" {
+  description = "IDs of the created null resources (count = 2)"
+  value       = [for r in null_resource.example : r.id]
+}
